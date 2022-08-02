320a90b7ff66a231b8e4826687decec765eafeba
Andreas Kohlbecker <kohlbecker@zkm.de>
2022-08-02 18:37:16 +0200

    updating to python3

--- LDAP2VCard.py
+++ LDAP2VCard.py
@@ -18,52 +18,64 @@ LDAP_SEARCH_FILTER = "(&(objectCategory=person)(objectClass=user))"
 LDAP_RETRIEVE_ATTRIBUTES = ["sn","givenName","displayName","telephoneNumber","mobile","thumbnailPhoto","mail"]
 OUTPUT_DIR = "contacts"
 
-def getAttribute(data, aName):
-	if aName in data[0][1]:
-		v = data[0][1][aName][0].decode('utf-8', 'ignore')
-		if aName == "mobile" or aName == "telephoneNumber":
-			v = v.replace(" ", "")
-			v = v.replace(".", "")
-			v = v.replace("-", "")
-		return v.encode('utf-8', 'ignore')
-	return ""
 
-def getBinaryAttribute(data, aName):
-	if aName in data[0][1]:
-		return base64.b64encode(data[0][1][aName][0])
-	return ""
+def attribute(data, aName):
+    if aName in data[0][1]:
+        v = data[0][1][aName][0].decode('utf-8', 'ignore')
+        if v.startswith("'") & v.endswith("'"):
+            v = v[1:-1]
+        if aName == "mobile" or aName == "telephoneNumber":
+            v = v.replace(" ", "")
+            v = v.replace(".", "")
+            v = v.replace("-", "")
+        return v
+    return ""
 
-l = ldap.initialize(LDAP_URL)
-l.simple_bind_s(LDAP_USER,LDAP_PASSWORD)
+
+def binary_attribute(data, aName):
+    if aName in data[0][1]:
+        return base64.b64encode(data[0][1][aName][0])
+    return ""
+
+
+ldap_connection = ldap.initialize(LDAP_URL)
+ldap_connection.simple_bind_s(LDAP_USER, LDAP_PASSWORD)
 searchScope = ldap.SCOPE_SUBTREE
 
 try:
-	ldap_result_id = l.search(LDAP_BASEDN, searchScope, LDAP_SEARCH_FILTER, LDAP_RETRIEVE_ATTRIBUTES)
-	result_set = []
-	while 1:
-		i=i+1
-		result_type, result_data = l.result(ldap_result_id, 0)
-		if (result_data == []):
-			break
-		else:
-			sn = getAttribute(result_data,"sn")
-			gn = getAttribute(result_data,"givenName")
-			tel = getAttribute(result_data,"telephoneNumber")
-			mobile = getAttribute(result_data,"mobile")
-			image = getBinaryAttribute(result_data,"thumbnailPhoto")
-			email = getAttribute(result_data,"mail")
-			vcard = (
-				"BEGIN:VCARD\r\nVERSION:3.0\r\n"
-				"FN;CHARSET=UTF-8:%s %s\r\nN;CHARSET=UTF-8:%s;%s\r\nTEL;WORK;VOICE:%s\r\n"
-				"TEL;CELL;VOICE:%s\r\nPHOTO;ENCODING=BASE64;TYPE=JPEG:%s\r\n"
-				"TITLE;CHARSET=UTF-8:%s\r\nEMAIL;INTERNET:%s\r\n"
-				"END:VCARD\r\n"
-			)
-			fileName = OUTPUT_DIR+"/%s.vcf" % (sn+" "+gn)
-			oFile = open(fileName.decode('utf-8', 'ignore'), "w")
-			oFile.write(vcard % (gn,sn,sn,gn,tel,mobile,image,title,email))
-			oFile.close()
-			
-except ldap.LDAPError, e:
-	print e
-
+    ldap_result_id = ldap_connection.search(LDAP_BASEDN, searchScope, LDAP_SEARCH_FILTER, LDAP_RETRIEVE_ATTRIBUTES)
+    result_set = []
+    i = 0
+    fileName = OUTPUT_DIR + "/ldap-contacts.vcf"
+    oFile = open(fileName, "w")
+    while 1:
+        i = i + 1
+        result_type, result_data = ldap_connection.result(ldap_result_id, 0)
+        if not result_data:
+            break
+        else:
+            sn = attribute(result_data, "sn")
+            gn = attribute(result_data, "givenName")
+            tel = attribute(result_data, "telephoneNumber")
+            mobile = attribute(result_data, "mobile")
+            image = binary_attribute(result_data, "thumbnailPhoto")
+            email = attribute(result_data, "mail")
+            title = ""  # TODO
+            vcard = (
+                "BEGIN:VCARD\r\n"
+                "VERSION:3.0\r\n"
+                "FN;CHARSET=UTF-8:%s %s\r\n"
+                "N;CHARSET=UTF-8:%s;%s\r\n"
+                "TEL;WORK;VOICE:%s\r\n"
+                "TEL;CELL;VOICE:%s\r\n"
+                "PHOTO;ENCODING=BASE64;TYPE=JPEG:%s\r\n"
+                "TITLE;CHARSET=UTF-8:%s\r\n"
+                "EMAIL;INTERNET:%s\r\n"
+                "END:VCARD\r\n"
+            )
+            print(vcard % (gn, sn, sn, gn, tel, mobile, image, title, email))
+            oFile.write(vcard % (gn, sn, sn, gn, tel, mobile, image, title, email))
+except ldap.LDAPError as e:
+    print(e)
+finally:
+    oFile.close()
